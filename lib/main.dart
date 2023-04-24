8fead27f75fde187648787af0bee699e5c742c2c
AbhishekSatpathy4848 <abhisheksatpathy.211it002@nitk.edu.in>
2023-04-24 16:49:43 +0530

    added files

---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(
+    home: SodukuSolver(),
+  ));
+}
+
+class SodukuSolver extends StatefulWidget {
+  const SodukuSolver({Key? key}) : super(key: key);
+
+  @override
+  State<SodukuSolver> createState() => _SodukuSolverState();
+}
+
+class _SodukuSolverState extends State<SodukuSolver> {
+  @override
+  void initState() {
+    super.initState();
+    fetchSodukuPuzzle();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Soduku Solver"),
+      ),
+      body: MaterialButton(
+        onPressed: () {},
+        color: Colors.red,
+      ),
+    );
+  }
+}
---
+++ lib/soduku_node.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class SudokuNode {
+  int value;
+  Color? color;
+  SudokuNode({required this.color,required this.value});
+}
